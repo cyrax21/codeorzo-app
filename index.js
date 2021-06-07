743540ba3f8e66363449f21da641e257b02599f0
Joy-deb <19bcs2264@cuchd.in>
2021-06-07 08:14:57 +0530

    Setting view engine as ejs

--- index.js
+++ index.js
@@ -5,7 +5,11 @@ const port = 8000;
 // Use Express Router
 app.use('/', require('./routes'));
 
+//setting up the view engine (ejs)
+app.set('view engine', 'ejs');
+app.set('view', './views');
 
+//Listening to the port
 app.listen(port, function(err){
     if(err) {
         console.log(`Error in running the server : ${err}`);
