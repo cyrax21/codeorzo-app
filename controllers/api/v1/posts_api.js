7153173d1065697a529fee21a0979d9ccbd095f4
Joy-deb <19bcs2264@cuchd.in>
2021-06-15 11:07:56 +0530

    Implemented few APIs in Project

---
+++ controllers/api/v1/posts_api.js
@@ -0,0 +1,43 @@
+const Post = require('../../../model/post');
+const Comment = require('../../../model/comment');
+
+module.exports.index = async function (req, res) {
+
+    let posts = await Post.find({})
+        .sort('-createdAt')     // used to sort the post in recent to oldest 
+        .populate('user')
+        .populate({
+            path: 'comments',
+            populate: {
+                path: 'user'
+            }
+        })
+
+
+    return res.json(200, {
+        message: "List of posts",
+        post: posts
+    })
+};
+
+module.exports.destroy = async function(req, res){
+
+    try{
+        let post = await Post.findById(req.params.id);
+        post.remove(); // Used to remove the Object from data
+
+        //  Deleting the comments in the post
+        // We are passing post id as query for deleting comments
+        await Comment.deleteMany({ post: req.params.id });
+
+        return res.json(200, {
+            message: "Post and associated comment deleted successfully",
+        })
+    }catch(err){
+        console.log("****", err);
+        res.status(500).json({
+            message: "Internal Server Error !"
+        })
+        return;
+    }
+}
--- routes/index.js
+++ routes/index.js
@@ -14,4 +14,6 @@ router.use('/posts', require('./posts'));
 // Added Router for comments related routes
 router.use('/comments', require('./comments'));
 
+router.use('/api', require('./api'));
+
 module.exports = router;
